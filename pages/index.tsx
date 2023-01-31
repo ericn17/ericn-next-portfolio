0ed803a8791d6597903b6536fa0b94dd9ff73d54
Eric Nguyen <15enguyen4@gmail.com>
2023-01-31 14:27:15 -0800

    bg-circle created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import Header from '@/components/Header'
+import Hero from '@/components/Hero'
 
 
 
@@ -8,7 +9,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Eric's Portfolio</title>    
       </Head>
@@ -16,7 +17,9 @@ export default function Home() {
       {/* Header */}
       <Header/>
       {/* Hero */}
-
+      <section id="hero" className='snap-center'>
+        <Hero/>
+      </section>
       {/* About */}
 
       {/* Experience */}
@@ -26,6 +29,6 @@ export default function Home() {
       {/* Projects */}
 
       {/* Contact Me */}
-    </>
+    </div>
   )
 }
