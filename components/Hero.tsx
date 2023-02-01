e574346d8288abd091dd4bf4e501407f855310b1
Eric Nguyen <15enguyen4@gmail.com>
2023-02-01 15:20:08 -0800

    bg-circle changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './BackgroundCircles';
+import Image from 'next/image';
+import ProfilePicture from 'public/profile-pic.jpg'
+import Link from 'next/link';
+
 
 type Props = {}
 
@@ -18,10 +22,35 @@ export default function Hero({}: Props) {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden ">
         <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor='#F7AB0A' />
-      </h1>
+        <Image 
+          className='relative rounded-full h-32 w-32 mx-auto object-cover'
+          src={ProfilePicture}
+          alt="profile picture"  
+        />
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
+          Web Developer
+        </h2>
+        <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+          <span>{text}</span>
+          <Cursor cursorColor='#F7AB0A' />
+        </h1>
+
+        <div className='pt-5'>
+          <Link href='#about'>
+          <button className='heroButton'>About</button>
+          </Link>
+          <Link href='#experience'>
+          <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href='#skills'>
+          <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href='#projects'>
+          <button className='heroButton'>Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
