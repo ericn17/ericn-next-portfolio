af0f201538b786ea3c2812ff326f730be27e4029
Eric Nguyen <15enguyen4@gmail.com>
2023-02-03 13:42:42 -0800

    about section set up

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+
 
 type Props = {}
 
@@ -9,6 +11,25 @@ export default function About({}: Props) {
         About
       </h3>
 
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+        className='mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+        src=''
+        alt='profilepic'
+      />
+
+      <div className='space-y-10 px-0 md:px-10'>
+        <h4 className='text-4xl font-semibold'>Here is a little background</h4>
+        <p></p>
+      </div>
     </div>
   )
 }
