0964e99ecb4da224d4b6fea022536b7bfbe0f2ae
Eric Nguyen <15enguyen4@gmail.com>
2023-02-08 19:34:56 -0800

    experience section added

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import Image from 'next/image'
 
 
 type Props = {}
@@ -27,12 +28,12 @@ export default function About({}: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         className='mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
-        src=''
+        src='/profile-pic.jpg'
         alt='profilepic'
       />
 
       <div className='space-y-10 px-0 md:px-10'>
-        <h4 className='text-4xl font-semibold'>Here is a little background</h4>
+        <h4 className='text-4xl font-semibold'>Here is a <span className='underline decoration-[#FCEE0A]/80'>little</span> background</h4>
         <p>I graduated from Cal State Fullerton with a degree in finance but ultimately fell in love with coding and enrolled in the UCI Coding Bootcamp. There my interest for coding grew as well as my ability to problem solve. Learning to code has been a challenging but very rewarding journey. I grew up gaming and so the my portfolio's theme is Cyber Punk inspired.</p>
       </div>
     </motion.div>
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
 
 type Props = {}
 
 export default function Experience({}: Props) {
   return (
-    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <motion.div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-[#CDCDCD] text-2xl'>
         Experience
       </h3>
-    </div>
+
+      <motion.img
+        initial={{
+          x: -200,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1.2,
+        }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true }}
+        className='mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-fit md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+        src='/uci-bc-logo.png'
+        alt='ucibootcamp'
+      />
+
+      <div className='space-y-10 px-0 md:px-10'>
+        <h4 className='text-4xl font-semibold'>Coding Bootcamp</h4>
+        <p>I enrolled in the UCI, Division of Continuing Education, boot camp in May of 2022. Through the UCI Coding bootcamp, they stimulated a professional work environment which required me to work closely with my peers to build real-world projects and complex applications. My group collaborated together on a single respository similar to a professional work environment and there we would debug and decide which changes were commited.  </p>
+      </div>
+    </motion.div>
   )
 }
