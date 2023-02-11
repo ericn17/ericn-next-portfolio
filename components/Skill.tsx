db8862006b56cfaa4be9ce0236b78d64efb50912
Eric Nguyen <15enguyen4@gmail.com>
2023-02-11 13:36:19 -0800

    setting up skills section with grid to display each skill neatly

---
+++ components/Skill.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+type Props = {}
+
+export default function Skill({}: Props) {
+  return (
+    <div className=''>
+      <motion.img
+        
+      />
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Experience from '@/components/Experience'
+import Skills from '@/components/Skills'
 
 
 
@@ -17,22 +18,24 @@ export default function Home() {
         <title>Eric's Portfolio</title>    
       </Head>
 
-      {/* Header */}
       <Header/>
-      {/* Hero */}
+   
       <section id="hero" className='snap-center'>
         <Hero/>
       </section>
-      {/* About */}
+    
       <section id='about' className='snap-center'>
         <About/>
       </section>
-      {/* Experience */}
+
       <section id='experience' className='snap-center'>
         <Experience/>
       </section>
-      {/* Skills */}
 
+      {/* Skills */}
+      <section id='skills' className='snap-center'>
+        <Skills/>
+      </section>
       {/* Projects */}
 
       {/* Contact Me */}
