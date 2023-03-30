b6dec38b0532242f7ea972630c5bf357f3b6954c
Eric Nguyen <15enguyen4@gmail.com>
2023-03-30 12:06:05 -0700

    project sanity data implemented with technologies used

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -40,6 +40,17 @@ export default function Projects({ projects }: Props) {
               {project.title}
             </h4>
 
+            <div className='flex items-center space-x-2 justify-center'>
+              {project?.technologies.map(technology => (
+                <img 
+                  className="h-10 w-10"
+                  key={technology._id}
+                  src={urlFor(technology.image).url()}
+                  alt=""
+                />
+                ))}
+            </div>
+
             <p className='text-lg text-center md:text-left'>
               {project.summary}
             </p>
